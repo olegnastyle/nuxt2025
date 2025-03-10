2300f9a7ab664b98a1c59fb3fd214f87d8219f2f
olegnastyle <olegnastyle@yandex.ru>
2025-03-10 20:48:48 +0300

    upd * первый блок в index.vue

--- app.vue
+++ app.vue
@@ -13,15 +13,4 @@ useHead({
     class: 'bg-white dark:bg-gray-900'
   },
 })
-
-// if (import.meta.server) {
-//   // These meta tags will only be added during server-side rendering
-//   useSeoMeta({
-//     title: 'fefe',
-//     robots: 'index, follow',
-//     description: 'Static description that does not need reactivity',
-//     ogImage: 'https://example.com/image.png',
-//     // other static meta tags...
-//   })
-// }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1 class="my-4 text-2xl text-blue-700 dark:blue-500 font-medium">Главная страница</h1>
+  <!-- первый блок -->
+  <section class="flex flex-col lg:flex-row items-center gap-24">
+    <!-- левая сторона -->
+    <div class="flex flex-col gap-4 text-black dark:text-white">
+      <h1 class="text-6xl uppercase">Самый <span class="text-blue-700 text-7xl font-bold ">космические</span> рецепты от Шефа</h1>
+      <p class="text-2xl">Вдохновляйтесь кулинарными шедеврами от профессионального шеф-повара и создавайте настоящие кулинарные произведения искусства на своей кухне!</p>
+    </div>
+    <!-- правая сторона -->
+    <div class="grid grid-cols-2 grid-rows-2 items-center gap-8">
+      <article v-for="(post, index) in posts" :key="post.id" :class="index === 0 ? 'row-span-2 *:*:h-80 *:*:w-3xs' : ''">
+        <NuxtLink :to="`/${post.category?.slug}/${post.slug}`">
+          <img class="object-cover rounded-3xl" :src="'http://localhost:1337'+post.cover.url" :alt="post.cover.alternativeText">
+        </NuxtLink>
+      </article>
+    </div>
+  </section>
+
 </template>
 
 <script setup>
+const posts = ref([])
 const index = useIndexStore();
+
+const fetch = async () => {
+    try {
+        // включаем loader
+        index.loader = true;
+
+        const res = await $fetch('http://localhost:1337/api/posts?populate=*')
+
+        return posts.value = res.data
+    } catch (error) {
+        console.log(error);
+    } finally {
+        // выключаем loader
+        index.loader = false;
+    }
+}
 
 const seo = ref({})
 const fetchSeo = async () => {
@@ -29,5 +62,8 @@ const fetchSeo = async () => {
   }
 };
 
-onMounted(() => fetchSeo())
+onMounted(() => {
+  fetch()
+  fetchSeo()
+})
 </script>
