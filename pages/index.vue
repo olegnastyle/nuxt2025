d2b9eb034e2689e8bdb3b55dad5672732777cf45
olegnastyle <olegnastyle@yandex.ru>
2025-03-12 19:55:33 +0300

    upd * duble block

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,13 @@
   <section class="flex flex-col lg:flex-row items-center gap-24">
     <!-- левая сторона -->
     <div class="flex flex-col gap-4 text-black dark:text-white">
-      <h1 class="text-6xl uppercase">Самый <span class="text-blue-700 text-7xl font-bold ">космические</span> рецепты от Шефа</h1>
+      <h1 class="text-5xl font-medium uppercase">Самый <span class="text-blue-700 text-6xl font-bold ">космические</span> рецепты от Шефа</h1>
       <p class="text-2xl">Вдохновляйтесь кулинарными шедеврами от профессионального шеф-повара и создавайте настоящие кулинарные произведения искусства на своей кухне!</p>
     </div>
     <!-- правая сторона -->
     <div class="grid grid-cols-2 grid-rows-2 items-center gap-8">
-      <article v-for="(post, index) in posts" :key="post.id" :class="index === 0 ? 'row-span-2 *:*:h-80 *:*:w-3xs' : ''">
+      <article v-for="(post, index) in posts" :key="post.id" :class="index === 0 ? 'row-span-2 *:*:h-80 *:*:w-3xs' : ''"
+        class="hover:scale-105 transition-transform">
         <NuxtLink :to="`/${post.category?.slug}/${post.slug}`">
           <img class="object-cover rounded-3xl" :src="'http://localhost:1337'+post.cover.url" :alt="post.cover.alternativeText">
         </NuxtLink>
@@ -16,6 +17,29 @@
     </div>
   </section>
 
+  <!-- второй блок -->
+  <section class="text-black dark:text-white">
+    <h2 class="my-1.5 text-3xl font-medium uppercase">Популярные статьи</h2>
+    <nav class="my-3">
+      <ul class="flex gap-4">
+        <li v-for="item in 10">
+          <NuxtLink to="/">Дома и быт</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <!-- список популярные статей -->
+    <div class="flex justify-between">
+      <article v-for="item in 4">
+        <NuxtLink to="/">
+          <img class="w-60 h-80 object-cover rounded-xl" src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&h=1000&q=80" alt="">
+        </NuxtLink>
+        <NuxtLink to="/">
+          <h3>Название статьи</h3>
+        </NuxtLink>
+        <NuxtLink to="/">Название категории</NuxtLink>
+      </article>
+    </div>
+  </section>
 </template>
 
 <script setup>
