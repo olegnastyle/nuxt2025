f6b45669651c247690896f64279daec483332ac7
gitlisso <lisso@nastyle.pro>
2025-03-28 01:30:52 +0300

    Рекламный баннер

--- components/ads.vue
+++ components/ads.vue
@@ -1,5 +1,8 @@
 <template>
-    <div v-if="view" class="w-full">
+    <div v-if="view" class="relative w-full">
+        <button @click="view = false" class="absolute top-8 right-8 z-50 cursor-pointer">
+            <svg class="size-8 text-2xl text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"></path></svg>
+        </button>
         <NuxtLink to="https://craftum.com/?id=629285&ref=pluspixel" target="_blank">
             <img src="~/public/banner-craftum-dark-lg.png" alt="Craftum" class="object-cover w-full">
         </NuxtLink>
@@ -7,11 +10,11 @@
 </template>
 
 <script setup>
-const view = ref(true)
+const view = ref(true);
 
 onMounted(() => {
     setInterval(() => {
         view.value = !view.value
     }, 150000) // 2,5 минуты в миллисекундах
-})
+});
 </script>
