a03d41058860d98ee5181ee4cddd15c71f6e9402
olegnastyle <olegnastyle@yandex.ru>
2025-03-31 21:18:17 +0300

    Вывели данные пользователя, форма авторизации видна если нет documentId

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,4 +1,15 @@
 <template>
     <h1 class="my-4 text-2xl text-blue-700 darl:blue-500 font-medium">Профиль</h1>
-    <AppAuth />
+    <!-- форма авторизации видна если нет documentId -->
+    <AppAuth v-if="!index.userMe.documentId" />
+    <!-- вывели данные пользователя -->
+    <ul class="dark:text-white/50 text-xl">
+        <li>Логин: <span class="text-white">{{ index.userMe.username }}</span></li>
+        <li>Почта: <span class="text-white">{{ index.userMe.email }}</span></li>
+        <li>Дата создания: <span class="text-white">{{ index.userMe.createdAt }}</span></li>
+    </ul>
 </template>
+
+<script setup>
+const index = useIndexStore();
+</script>
