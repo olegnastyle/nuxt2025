b6654b0853fcd34d296dda26cc7771961fdc0d0a
olegnastyle <olegnastyle@yandex.ru>
2025-04-02 20:03:33 +0300

    **

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -16,14 +16,14 @@
             </div>
             <div>
                 <!-- данные пользователя -->
-                <div v-if="editProfileToggle">
+                <div v-if="index.editProfileToggle">
                     <ul class="flex flex-col gap-2.5 text-black/50 dark:text-white text-sm">
                         <li>Логин:<br/><span class="text-black dark:text-white/50 text-xl">{{ index.userMe.username }}</span></li>
                         <li>Имя:<br/><span class="text-black dark:text-white/50 text-xl">{{ index.userMe.name }}</span></li>
                         <li>Почта:<br/><span class="text-black dark:text-white/50 text-xl">{{ index.userMe.email }}</span></li>
                         <li>Дата создания:<br/><span class="text-black dark:text-white/50 text-xl">{{ formatDate(index.userMe.createdAt) }}</span></li>
                     </ul>
-                    <button @click="editProfileToggle = false" class="flex items-center gap-1.5 py-3 px-6 mt-3 text-black dark:text-white text-sm font-light border border-black/50 dark:border-white/50 opacity-50 hover:opacity-100 rounded-xl cursor-pointer transition-opacity">
+                    <button @click="index.editProfileToggle = false" class="flex items-center gap-1.5 py-3 px-6 mt-3 text-black dark:text-white text-sm font-light border border-black/50 dark:border-white/50 opacity-50 hover:opacity-100 rounded-xl cursor-pointer transition-opacity">
                         <svg class="size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M16.7574 2.99678L14.7574 4.99678H5V18.9968H19V9.23943L21 7.23943V19.9968C21 20.5491 20.5523 20.9968 20 20.9968H4C3.44772 20.9968 3 20.5491 3 19.9968V3.99678C3 3.4445 3.44772 2.99678 4 2.99678H16.7574ZM20.4853 2.09729L21.8995 3.5115L12.7071 12.7039L11.2954 12.7064L11.2929 11.2897L20.4853 2.09729Z"></path></svg>
                         Редактировать профиль
                     </button>
