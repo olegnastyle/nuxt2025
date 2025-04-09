d37e290fb0ace66b732576b072dfb5af28db568b
olegnastyle <olegnastyle@yandex.ru>
2025-04-09 18:41:11 +0300

    **

--- plugins/theme.js
+++ plugins/theme.js
@@ -1,10 +1,10 @@
-export default () => {
-    if (import.meta.client) { // Проверяем, что код выполняется на клиенте
-      const savedTheme = localStorage.getItem('theme')
-      if (savedTheme === 'dark') {
-        document.documentElement.classList.add('dark')
-      } else {
-        document.documentElement.classList.remove('dark')
-      }
+export default defineNuxtPlugin(nuxtApp => {
+  if (import.meta.client) {
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
     }
   }
+})
