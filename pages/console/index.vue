cb92ae2ad3b1f74c711e4e244163794839500a2d
olegnastyle <olegnastyle@yandex.ru>
2025-04-09 20:17:49 +0300

    **

--- pages/console/index.vue
+++ pages/console/index.vue
@@ -5,6 +5,13 @@
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
       <!-- Управление блоками -->
       <BlockVisibilityControl />
+
+      <div class="p-4 bg-white dark:bg-gray-800 rounded-lg shadow">
+        <h2 class="text-lg font-semibold mb-4 text-gray-900 dark:text-white">Управление навигацией</h2>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
       
       <!-- Другие элементы управления -->
       <div class="p-4 bg-white dark:bg-gray-800 rounded-lg shadow">
@@ -22,7 +29,21 @@ import BlockVisibilityControl from '~/components/ui/BlockVisibilityControl.vue'
 
 const globalStore = useGlobalStore()
 
+const navbar = ref([])
+const fetchNavbar = async () => {
+    try {
+        index.loader = true
+        const res = await $fetch('https://static.dublecode.ru/api/navbars?sort=sort:asc')
+        navbar.value = res.data
+    } catch (error) {
+        console.log(error)
+    } finally {
+        index.loader = false
+    }
+}
+
 onMounted(async () => {
   await globalStore.fetchGlobalSettings()
+  fetchNavbar()
 })
 </script>
